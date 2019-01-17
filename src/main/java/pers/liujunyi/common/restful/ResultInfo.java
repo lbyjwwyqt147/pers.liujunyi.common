a640bbe3752688144871cee294961965a0bd9a2c
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2019-01-17 14:49:18 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/pers/liujunyi/common/configuration/DateConverterConfig.java

--- src/main/java/pers/liujunyi/common/restful/ResultInfo.java
+++ src/main/java/pers/liujunyi/common/restful/ResultInfo.java
@@ -17,12 +17,18 @@ public class ResultInfo implements Serializable {
 
     private static final long serialVersionUID = -4450312255234324795L;
 
-    private String status;  //状态码
-    private String message; // 描述信息
-    private Object data;   // 数据
-    private Object extend; //扩展数据
-    private String timestamp; //　时间
-    private Boolean success = true; // 是否处理成功
+    /** 状态码 */
+    private String status;
+    /** 描述信息 */
+    private String message;
+    /** 数据集 */
+    private Object data;
+    /** 扩展数据 */
+    private Object extend;
+    /** 时间 */
+    private String timestamp;
+    /** 是否处理成功 */
+    private Boolean success = true;
     @JSONField(serialize = false)
     @JsonIgnore
     private ErrorCodeEnum errorCodeEnum;
