8be597d620dfc085b90c5b8226ce78fb4e2316df
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2019-03-11 00:20:39 +0800

    repository

--- src/main/java/pers/liujunyi/common/exception/GlobalExceptionHandler.java
+++ src/main/java/pers/liujunyi/common/exception/GlobalExceptionHandler.java
@@ -17,8 +17,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import pers.liujunyi.common.restful.ResultInfo;
 import pers.liujunyi.common.restful.ResultUtil;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.ValidationException;
 import java.util.List;
+import java.util.Set;
 
 /***
  * 异常处理
@@ -35,7 +38,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = DescribeException.class)
     @ResponseBody
     public ResultInfo handlerDescribeException(DescribeException e) {
-        log.error("【系统逻辑异常】： ", e);
+        log.error("【系统业务逻辑异常】： ", e);
         return ResultUtil.error(ErrorCodeEnum.ERROR.getCode(), e.getMessage());
     }
 
@@ -51,7 +54,7 @@ public class GlobalExceptionHandler {
             DescribeException myException = (DescribeException) e;
             return ResultUtil.error(ErrorCodeEnum.ERROR.getCode(), myException.getMessage());
         }
-        log.error("【系统逻辑异常】： ", e);
+        log.error("【系统异常】： ", e);
         return ResultUtil.error();
     }
 
@@ -81,14 +84,17 @@ public class GlobalExceptionHandler {
     public ResultInfo handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         StringBuffer errorMsg = new StringBuffer();
         BindingResult result = e.getBindingResult();
-        result.getAllErrors().forEach(item -> errorMsg.append(item.getDefaultMessage()).append("."));
+        result.getAllErrors().stream().forEach(item -> errorMsg.append(item.getDefaultMessage()).append("."));
         log.error("【参数验证失败】： ", e);
         log.info(errorMsg.toString());
         return ResultUtil.error(ErrorCodeEnum.PARAMS.getCode(), errorMsg.toString());
     }
 
+
     /**
      * 405 - Method Not Allowed
+     * @param e
+     * @return
      */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     @ResponseBody
@@ -98,17 +104,29 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 400 - Bad Request
+     * 捕获校验参数异常方法
+     * @param exception
+     * @return
      */
     @ExceptionHandler(ValidationException.class)
     @ResponseBody
-    public ResultInfo handleValidationException(ValidationException e) {
-        log.error("【参数验证失败】", e);
+    public ResultInfo handleValidationException(ValidationException exception) {
+        log.error("【参数验证失败】",exception);
+        if (exception instanceof ConstraintViolationException){
+            StringBuffer errorMsg = new StringBuffer();
+            ConstraintViolationException exs = (ConstraintViolationException) exception;
+            Set<ConstraintViolation<?>> violations = exs.getConstraintViolations();
+            violations.stream().forEach(item -> errorMsg.append(item.getMessage()).append("."));
+            log.info(errorMsg.toString());
+            return  ResultUtil.error(HttpStatus.BAD_REQUEST.value(), errorMsg.toString());
+        }
         return  ResultUtil.error(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase());
     }
 
     /**
      * 400 - Bad Request
+     * @param e
+     * @return
      */
     @ExceptionHandler(MissingServletRequestParameterException.class)
     @ResponseBody
@@ -119,6 +137,8 @@ public class GlobalExceptionHandler {
 
     /**
      * 400 - Bad Request
+     * @param e
+     * @return
      */
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseBody
