cdcf2bcb04fece12b1add7947e77d630f532663e
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2019-05-22 20:43:58 +0800

    repository

--- src/main/java/pers/liujunyi/cloud/common/service/BaseService.java
+++ src/main/java/pers/liujunyi/cloud/common/service/BaseService.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Sort;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 /***
  * 文件名称: BaseService.java
@@ -18,7 +17,7 @@ import java.util.Map;
  * @version 1.0
  * @author ljy
  */
-public interface BaseService<T, PK extends Serializable> {
+public interface BaseService<T, PK extends Serializable> extends ElasticsearchBaseService<T, PK> {
 
     /**
      * 批量插入
@@ -101,63 +100,5 @@ public interface BaseService<T, PK extends Serializable> {
      * @return
      */
     List<T> findByIdInOrderByIdAsc(List<PK> ids);
-
-    /**
-     * 单条更新 Elasticsearch 里面的字段数据  参数为Map<String,Object>
-     * @param id  id
-     * @param sourceMap  资源   key = 需要更新的字段  value = 字段值
-     * @return true 成功   false  失败
-     */
-    Boolean updateSingleElasticsearchData(Long id, Map<String, Object> sourceMap);
-
-    /**
-     * 批量更新 Elasticsearch 里面的字段数据  参数为Map<String,Object>
-     * @param sourceMap  key = id  value = Map<String, Object> key = 需要更新的字段  value = 字段值
-     */
-    void updateBatchElasticsearchData(Map<String, Map<String, Object>> sourceMap);
-
-    /**
-     * 保存索引数据到 Elasticsearch 中  参数为Map<String,Object>
-     * @param id  索引id
-     * @param sourceMap   资源   key = 字段  value = 字段值
-     * @return
-     */
-    Boolean saveElasticsearchIndexMapData(Long id, Map<String, Object> sourceMap);
-
-    /**
-     * 保存索引数据到 Elasticsearch 中   参数为javaBean
-     * @param id  索引id
-     * @param t   资源   javaBean
-     * @return
-     */
-    Boolean saveElasticsearchIndexBeanData(Long id, T t);
-
-    /**
-     * 批量报错索引数据到 Elasticsearch 中   参数为javaBean
-     * @param source  参数为Map<String,Object>
-     */
-    void saveBatchElasticsearchIndexBeanData(Map<String, T> source);
-
-    /**
-     * 保存索引数据到 Elasticsearch 中  资源  为 json 字符串
-     * @param id  索引id
-     * @param sourceJson   资源  json 字符串
-     * @return
-     */
-    Boolean saveElasticsearchIndexJsonData(Long id, String sourceJson);
-
-
-    /**
-     * 单条删除 Elasticsearch 中  索引数据
-      * @param id 索引id
-     * @return
-     */
-    Boolean deleteSingleElasticsearchIndex(Long id);
-
-    /**
-     * 批量删除 Elasticsearch 中  索引数据
-     * @param ids  一组 索引id
-     */
-    void  deleteBatchElasticsearchIndex(List<Long> ids);
 
 }
--- src/main/java/pers/liujunyi/cloud/common/service/impl/BaseServiceImpl.java
+++ src/main/java/pers/liujunyi/cloud/common/service/impl/BaseServiceImpl.java
@@ -1,36 +1,21 @@
 package pers.liujunyi.cloud.common.service.impl;
 
 import lombok.extern.log4j.Log4j2;
-import org.apache.commons.lang3.StringUtils;
-import org.elasticsearch.action.bulk.BulkItemResponse;
-import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.action.delete.DeleteRequestBuilder;
-import org.elasticsearch.action.index.IndexRequestBuilder;
-import org.elasticsearch.action.update.UpdateRequestBuilder;
-import org.elasticsearch.client.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 import pers.liujunyi.cloud.common.repository.jpa.BaseRepository;
 import pers.liujunyi.cloud.common.service.BaseService;
 
-import javax.annotation.Resource;
 import java.io.Serializable;
-import java.lang.reflect.ParameterizedType;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /***
  * 文件名称: BaseServiceImpl.java
@@ -44,9 +29,7 @@ import java.util.Map;
  * @author ljy
  */
 @Log4j2
-public class BaseServiceImpl<T, PK extends Serializable> implements BaseService<T, PK> {
-
-    protected Class <T> tClazz  = (Class <T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+public class BaseServiceImpl<T, PK extends Serializable> extends ElasticsearchBaseServiceImpl<T, PK> implements BaseService<T, PK> {
 
     /** 自定义分页数据  */
     protected  Pageable pageable;
@@ -59,8 +42,6 @@ public class BaseServiceImpl<T, PK extends Serializable> implements BaseService<
     protected BaseRepository<T, PK> baseRepository;
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    @Resource
-    private ElasticsearchTemplate elasticsearchTemplate;
 
     public BaseServiceImpl(final BaseRepository<T, PK> baseRepository) {
         this.baseRepository = baseRepository;
@@ -139,214 +120,6 @@ public class BaseServiceImpl<T, PK extends Serializable> implements BaseService<
         return this.baseRepository.findByIdInOrderByIdAsc(ids);
     }
 
-    @Override
-    public Boolean updateSingleElasticsearchData(Long id, Map<String, Object> sourceMap) {
-        boolean success = false;
-        if (!CollectionUtils.isEmpty(sourceMap)) {
-            Document annotation = this.getDocumentAnnotation();
-            String indexName = annotation.indexName().trim();
-            String type = annotation.type().trim();
-            StringBuffer msg = new StringBuffer();
-            msg.append("indexName:").append(indexName).append(" type:").append(type);
-            log.info(" 开始 更新 Elasticsearch   " + msg.toString() +"   里面的字段数据 ........... ");
-            Client elasticsearchClient = this.elasticsearchTemplate.getClient();
-            UpdateRequestBuilder updateRequestBuilder = elasticsearchClient.prepareUpdate(annotation.indexName(), annotation.type(), String.valueOf(id));
-            updateRequestBuilder.setDoc(sourceMap);
-            if (updateRequestBuilder.execute().actionGet() != null) {
-                success = true;
-                log.info("  更新 Elasticsearch " + msg.toString() + " 里面的字段数据 成功! ");
-            } else {
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info("  更新 Elasticsearch " + msg.toString() + " 里面的字段数据 失败! ");
-            }
-        }
-        return success;
-    }
-
-    @Override
-    public void updateBatchElasticsearchData(Map<String, Map<String, Object>> sourceMap) {
-        if (!CollectionUtils.isEmpty(sourceMap)) {
-            Document annotation = this.getDocumentAnnotation();
-            String indexName = annotation.indexName().trim();
-            String type = annotation.type().trim();
-            StringBuffer msg = new StringBuffer();
-            msg.append("indexName:").append(indexName).append(" type:").append(type);
-            log.info(" 开始 更新 Elasticsearch   " + msg.toString() +"   里面的字段数据 ........... ");
-            Client elasticsearchClient = this.elasticsearchTemplate.getClient();
-            BulkRequestBuilder bulkRequest = elasticsearchClient.prepareBulk();
-            Iterator<Map.Entry<String, Map<String, Object>>> entries = sourceMap.entrySet().iterator();
-            while (entries.hasNext()) {
-                Map.Entry<String, Map<String, Object>> entry = entries.next();
-                String id = entry.getKey();
-                Map<String, Object> source = entry.getValue();
-                bulkRequest.add(elasticsearchClient.prepareUpdate(annotation.indexName(), annotation.type(), id).setDoc(source));
-            }
-            // 批量执行
-            BulkResponse bulkResponse = bulkRequest.execute().actionGet();
-            if(bulkResponse.hasFailures()) {
-                BulkItemResponse[] items = bulkResponse.getItems();
-                for(BulkItemResponse item : items) {
-                    log.info(" 更新 Elasticsearch " + msg.toString() + " 里面的字段失败 .. 错误信息：" + item.getFailureMessage());
-                }
-            } else {
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info(" 更新 Elasticsearch " + msg.toString() + " 里面的字段数据 全部执行成功！");
-            }
-        }
-    }
-
-    @Override
-    public Boolean saveElasticsearchIndexMapData(Long id, Map<String, Object> sourceMap) {
-        return this.saveElasticsearchIndexData(id, sourceMap);
-    }
-
-    @Override
-    public Boolean saveElasticsearchIndexBeanData(Long id, T t) {
-        return this.saveElasticsearchIndexData(id, t);
-    }
-
-
-    @Override
-    public void saveBatchElasticsearchIndexBeanData(Map<String, T> source) {
-        if (!CollectionUtils.isEmpty(source)) {
-            Document annotation = this.getDocumentAnnotation();
-            String indexName = annotation.indexName().trim();
-            String type = annotation.type().trim();
-            StringBuffer msg = new StringBuffer();
-            msg.append("indexName:").append(indexName).append(" type:").append(type);
-            log.info(" 开始 添加 Elasticsearch   " + msg.toString() +"   数据 ........... ");
-            Client elasticsearchClient = this.elasticsearchTemplate.getClient();
-            BulkRequestBuilder bulkRequest = elasticsearchClient.prepareBulk();
-            Iterator<Map.Entry<String, T>> entries = source.entrySet().iterator();
-            while (entries.hasNext()) {
-                Map.Entry<String, T> entry = entries.next();
-                String id = entry.getKey();
-                T t = entry.getValue();
-                bulkRequest.add(elasticsearchClient.prepareIndex(annotation.indexName(), annotation.type(), String.valueOf(id)).setSource(t));
-            }
-            // 批量执行
-            BulkResponse bulkResponse = bulkRequest.execute().actionGet();
-            if(bulkResponse.hasFailures()) {
-                BulkItemResponse[] items = bulkResponse.getItems();
-                for(BulkItemResponse item : items) {
-                    log.info(" 添加 Elasticsearch " + msg.toString() + " 数据失败 .. 错误信息：" + item.getFailureMessage());
-                }
-            } else {
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info(" 添加 Elasticsearch " + msg.toString() + " 数据 全部执行成功！");
-            }
-        }
-    }
-
-    @Override
-    public Boolean saveElasticsearchIndexJsonData(Long id, String sourceJson) {
-        return this.saveElasticsearchIndexData(id, sourceJson);
-    }
-
-    @Override
-    public Boolean deleteSingleElasticsearchIndex(Long id) {
-        boolean success = false;
-        if (id != null) {
-            Document annotation = this.getDocumentAnnotation();
-            String indexName = annotation.indexName().trim();
-            String type = annotation.type().trim();
-            StringBuffer msg = new StringBuffer();
-            msg.append("indexName:").append(indexName).append(" type:").append(type).append(" index:").append(id);
-            log.info(" 开始 删除 Elasticsearch   " + msg.toString() +"   数据 ........... ");
-            Client elasticsearchClient = this.elasticsearchTemplate.getClient();
-            DeleteRequestBuilder deleteRequestBuilder  = elasticsearchClient.prepareDelete(annotation.indexName(), annotation.type(), String.valueOf(id));
-            if (deleteRequestBuilder.execute().actionGet() != null) {
-                success = true;
-                log.info("  删除 Elasticsearch " + msg.toString() + " 数据 成功! ");
-            } else {
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info("  删除 Elasticsearch " + msg.toString() + " 数据 失败! ");
-            }
-        }
-        return success;
-    }
-
-    @Override
-    public void deleteBatchElasticsearchIndex(List<Long> ids) {
-        if (!CollectionUtils.isEmpty(ids)) {
-            Document annotation = this.getDocumentAnnotation();
-            String indexName = annotation.indexName().trim();
-            String type = annotation.type().trim();
-            StringBuffer msg = new StringBuffer();
-            msg.append("indexName:").append(indexName).append(" type:").append(type).append(" index:").append(StringUtils.join(ids, ","));
-            log.info(" 开始 删除 Elasticsearch   " + msg.toString() +"   数据 ........... ");
-            Client elasticsearchClient = this.elasticsearchTemplate.getClient();
-            BulkRequestBuilder bulkRequest = elasticsearchClient.prepareBulk();
-            ids.stream().forEach(item -> {
-                bulkRequest.add(elasticsearchClient.prepareDelete(annotation.indexName(), annotation.type(), String.valueOf(item)));
-            });
-            // 批量执行
-            BulkResponse bulkResponse = bulkRequest.execute().actionGet();
-            if(bulkResponse.hasFailures()) {
-                BulkItemResponse[] items = bulkResponse.getItems();
-                for(BulkItemResponse item : items) {
-                    log.info(" 删除 Elasticsearch " + msg.toString() + " 数据失败 .. 错误信息：" + item.getFailureMessage());
-                }
-            } else {
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info(" 删除 Elasticsearch " + msg.toString() + " 数据 全部执行成功！");
-            }
-        }
-    }
-
-    /**
-     * 添加  Elasticsearch 数据
-     * @param id
-     * @param object
-     * @return
-     */
-    private Boolean saveElasticsearchIndexData(Long id, Object object) {
-        boolean success = false;
-        if (object != null) {
-            Document annotation = this.getDocumentAnnotation();
-            String indexName = annotation.indexName().trim();
-            String type = annotation.type().trim();
-            StringBuffer msg = new StringBuffer();
-            msg.append("indexName:").append(indexName).append(" type:").append(type);
-            log.info(" 开始 添加 Elasticsearch   " + msg.toString() +"   数据 ........... ");
-            Client elasticsearchClient = this.elasticsearchTemplate.getClient();
-            IndexRequestBuilder indexRequestBuilder  = elasticsearchClient.prepareIndex(annotation.indexName(), annotation.type(), String.valueOf(id));
-            indexRequestBuilder.setSource(object);
-            if (indexRequestBuilder.execute().actionGet() != null) {
-                success = true;
-                log.info("  添加 Elasticsearch " + msg.toString() + " 数据 成功! ");
-            } else {
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info("  添加 Elasticsearch " + msg.toString() + " 数据 失败! ");
-            }
-        }
-        return success;
-    }
-
     /**
      * 返回分页数据
      * @param pageSize
@@ -356,19 +129,4 @@ public class BaseServiceImpl<T, PK extends Serializable> implements BaseService<
         return PageRequest.of(0, pageSize);
     }
 
-    /**
-     * 获取 @Document
-     * @return
-     */
-    private Document getDocumentAnnotation() {
-        Document annotation = null;
-        try {
-            annotation =  this.tClazz.newInstance().getClass().getAnnotation(Document.class);
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return annotation;
-    }
 }
