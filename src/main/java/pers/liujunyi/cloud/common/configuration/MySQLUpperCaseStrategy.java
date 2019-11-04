d095da5eb8021b6e14cc63e8a2dc4a46c0d5854b
lbyjwwyqt147 <ljy671018>
2019-11-04 14:20:23 +0800

    字典

--- src/main/java/pers/liujunyi/cloud/common/configuration/MySQLUpperCaseStrategy.java
+++ src/main/java/pers/liujunyi/cloud/common/configuration/MySQLUpperCaseStrategy.java
@@ -47,13 +47,14 @@ public class MySQLUpperCaseStrategy extends SpringPhysicalNamingStrategy {
      * @return
      */
     private Identifier apply(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        if (name == null) {
+        if(name == null) {
             return null;
         } else {
+            //在大写字母前加下划线：LoginName --> Login_Name
             StringBuilder builder = new StringBuilder(name.getText().replace('.', '_'));
 
-            for (int i = 1; i < builder.length() - 1; ++i) {
-                if (this.isUnderscoreRequired(builder.charAt(i - 1), builder.charAt(i), builder.charAt(i + 1))) {
+            for(int i = 1; i < builder.length() - 1; ++i) {
+                if(this.isUnderscoreRequired(builder.charAt(i - 1), builder.charAt(i), builder.charAt(i + 1))) {
                     builder.insert(i++, '_');
                 }
             }
@@ -62,20 +63,21 @@ public class MySQLUpperCaseStrategy extends SpringPhysicalNamingStrategy {
         }
     }
 
+    @Override
     protected Identifier getIdentifier(String name, boolean quoted, JdbcEnvironment jdbcEnvironment) {
-        if (this.isCaseInsensitive(jdbcEnvironment)) {
-           // name = name.toLowerCase(Locale.ROOT);  //将所有字母转为小写
-            name = name.toUpperCase(Locale.ROOT);  //将所有字母转为大写
+        if(this.isCaseInsensitive(jdbcEnvironment)) {
+            name = name.toLowerCase(Locale.ROOT);
         }
-
         return new Identifier(name, quoted);
     }
 
+    @Override
     protected boolean isCaseInsensitive(JdbcEnvironment jdbcEnvironment) {
         return true;
     }
 
     private boolean isUnderscoreRequired(char before, char current, char after) {
+        //2.将大写字母变为小写：Login_Name--->login_name
         return Character.isLowerCase(before) && Character.isUpperCase(current) && Character.isLowerCase(after);
     }
 
