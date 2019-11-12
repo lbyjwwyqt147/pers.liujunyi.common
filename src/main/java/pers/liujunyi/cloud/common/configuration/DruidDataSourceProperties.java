905861a0f3974279298a6d65b0049fef6c318d57
lbyjwwyqt147 <ljy671018>
2019-11-12 14:44:03 +0800

    字典

---
+++ src/main/java/pers/liujunyi/cloud/common/configuration/DruidDataSourceProperties.java
@@ -0,0 +1,40 @@
+package pers.liujunyi.cloud.common.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author ljy
+ */
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource.druid")
+public class DruidDataSourceProperties {
+
+    private Integer initialSize;
+    private Integer maxActive;
+    private Integer maxWait;
+    private Integer minIdle;
+    private Long timeBetweenEvictionRunsMillis;
+    private Long minEvictableIdleTimeMillis;
+    private Integer maxOpenPreparedStatements;
+    private Boolean poolPreparedStatements;
+    private Integer maxPoolPreparedStatementPerConnectionSize;
+    private String validationQuery;
+    private Integer validationQueryTimeout;
+    private Integer queryTimeout;
+    private Long transactionThresholdMillis;
+    private Boolean testWhileIdle;
+    private Boolean testOnBorrow;
+    private Boolean testOnReturn;
+    private Boolean removeAbandoned;
+    private Integer removeAbandonedTimeoutMillis;
+    private Boolean logAbandoned;
+    private String filters;
+    private String connectionProperties;
+    private Boolean useGlobalDataSourceStat;
+}
