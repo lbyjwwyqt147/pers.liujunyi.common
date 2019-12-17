eb5c76119212605871eaf5c8aa81da8a3cf77aa6
lbyjwwyqt147 <ljy671018>
2019-12-17 17:12:19 +0800

    字典

--- src/main/java/pers/liujunyi/cloud/common/entity/TenementAuditListener.java
+++ src/main/java/pers/liujunyi/cloud/common/entity/TenementAuditListener.java
@@ -5,6 +5,7 @@ import pers.liujunyi.cloud.common.util.UserContext;
 import javax.persistence.PostPersist;
 import javax.persistence.PostRemove;
 import javax.persistence.PostUpdate;
+import java.util.Date;
 
 /**
  * @author ljy
@@ -31,6 +32,13 @@ public class TenementAuditListener {
     @PostUpdate
     private void PostUpdate(BaseEntity entity) {
         //更新数据库中的实体（在commit或期间flush）
+        // 设置租户ID
+        Long lesseeId = UserContext.currentTenementId();
+        if (lesseeId != null) {
+            entity.setTenementId(Long.valueOf(lesseeId));
+        }
+        entity.setUpdateTime(new Date());
+        entity.setUpdateUserId(UserContext.currentUserId());
     }
 
 }
