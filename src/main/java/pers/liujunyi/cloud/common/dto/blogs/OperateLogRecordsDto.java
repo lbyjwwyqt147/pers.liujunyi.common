23162f45a69269c84bd7fec51ce3bd6c14640044
lbyjwwyqt147 <ljy671018>
2020-03-25 15:22:13 +0800

    操作日志

---
+++ src/main/java/pers/liujunyi/cloud/common/dto/blogs/OperateLogRecordsDto.java
@@ -0,0 +1,64 @@
+package pers.liujunyi.cloud.common.dto.blogs;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import pers.liujunyi.cloud.common.dto.BaseDto;
+
+/***
+ * 文件名称: OperateLogRecordsDto.java
+ * 文件描述: 操作日志记录
+ * 公 司:
+ * 内容摘要:
+ *
+ * 完成日期:2019年03月11日
+ * 修改记录:
+ * @version 1.0
+ * @author ljy
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class OperateLogRecordsDto extends BaseDto {
+
+    /** 操作人id */
+    private Long operateUserId;
+
+    /** 操作人编号 */
+    private String operateUserNumber;
+
+    /** 操作人名称 */
+    private String operateUserName;
+
+    /** 用户类别   0：超级管理员 1：普通管理员 2：内部职工 3：普通用户   */
+    private Byte operateUserType;
+
+    /** 操作模块 */
+    private String operateModule;
+
+    /** 应用名称 */
+    private String applicationName;
+
+    /** 操作类型 */
+    private Byte operateType;
+
+    /** 日志类型 */
+    private Byte logType;
+
+    /** 操作的表名 */
+    private String tableName;
+
+    /** 操作方法名 */
+    private String operateMethod;
+
+    /** 说明 */
+    private String explain;
+
+    /** 参数 */
+    private String parameters;
+
+    /** 日志内容 */
+    private String logContent;
+
+    /** 操作状态 */
+    private Byte operateStatus;
+
+}
