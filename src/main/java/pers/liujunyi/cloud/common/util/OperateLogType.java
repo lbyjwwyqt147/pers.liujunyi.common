330b86ccad11a6d29fb62d09b1d487c335f77a22
lbyjwwyqt147 <ljy671018>
2020-03-30 17:02:22 +0800

    操作日志

---
+++ src/main/java/pers/liujunyi/cloud/common/util/OperateLogType.java
@@ -0,0 +1,14 @@
+package pers.liujunyi.cloud.common.util;
+
+/***
+ * 操作日志类型
+ */
+public abstract class OperateLogType {
+
+    /** 新增 */
+    public final static String ADD = "1";
+    /** 修改 */
+    public final static String UPDATE = "2";
+    /** 删除 */
+    public final static String DELETE = "3";
+}
